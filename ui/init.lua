b6d8acb45272ba991b3170e1d1e1448258c49356
shibinyeo <yeoshibin97@gmail.com>
2023-08-15 01:35:35 +0800

    feat: Layed out code structure for Top Panel

--- ui/init.lua
+++ ui/init.lua
@@ -11,7 +11,7 @@ local awful = require("awful")
 -- local decorations = require(... .. ".decorations")
 -- decorations.init()
  
--- local top_panel = require(... .. ".panels.top-panel")
+local top_panel = require(... .. ".panels.top-panel")
 -- local central_panel = require(... .. ".panels.central-panel")
 -- local info_panel = require(... .. ".panels.info-panel")
 -- local notification_panel = require(... .. ".panels.notification-panel")
@@ -24,7 +24,7 @@ local awful = require("awful")
 awful.screen.connect_for_each_screen(function(s)
 
 	--- Panels
-	-- top_panel(s)
+	top_panel(s)
 	-- central_panel(s)
 	-- info_panel(s)
 	-- notification_panel(s)
